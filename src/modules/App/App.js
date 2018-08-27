536244606a2e8694b241fd4fdb8dd146ce1d7728
Rowland Zhang <zhangh@gwmail.gwu.edu>
2018-08-27 01:01:23 -0400

    first draft

--- src/modules/App/App.js
+++ src/modules/App/App.js
@@ -4,7 +4,9 @@ import { Helmet } from 'react-helmet';
 import logo from '../../components/logo.svg';
 
 import Home from '../../pages/Home';
-import Directions from '../../pages/Directions';
+import About from '../../pages/About';
+import Services from '../../pages/Services';
+import Contact from '../../pages/Contact';
 import NotFound from '../../pages/404';
 
 export default class App extends Component {
@@ -15,32 +17,33 @@ export default class App extends Component {
         <Helmet>
           <title>Home | Cullen, Murphy & Co., PC.</title>
         </Helmet>
+        <div className="app-header">
+          <Link to='/'><img src={logo} className="app-logo" alt="logo" /></Link>
+          <div className="nav-bar">
+            <Link to='/' className="nav-item">Home</Link>
+            <Link to='/about' className="nav-item">About</Link>
+            <Link to='/services' className="nav-item">Services</Link>
+            <Link to='/contact' className="nav-item">Contact</Link>
+            <a href='http://portal.cullenmurphyandco.com/thinclient/login.aspx' className="nav-item" target='_blank' rel="noopener noreferrer">Client Portal</a>
+          </div>
+        </div>
         <div className="container-fluid">
-            <div className="app-header">
-              <Link to='/'><img src={logo} className="app-logo" alt="logo" /></Link>
-              <div className="nav-bar">
-                <Link to='/' className="nav-item">Home</Link>
-                <Link to='/' className="nav-item">About</Link>
-                <Link to='/' className="nav-item">Services</Link>
-                <Link to='/directions' className="nav-item">Directions</Link>
-                <a href='http://portal.cullenmurphyandco.com/thinclient/login.aspx' className="nav-item" target='_blank'>Client Portal</a>
-              </div>
-            </div>
             <div className="app-module">
             <Switch>
               <Route exact path='/' component={Home} />
-              <Route exact path='/index' component={Home} />
-              <Route exact path='/directions' component={Directions} />
+              <Route exact path='/about' component={About} />
+              <Route exact path='/contact' component={Contact} />
+              <Route exact path='/services' component={Services} />
               <Route component={NotFound} />
             </Switch>
             </div>
-            <div className="app-footer flood">
-              <div className="footer-info">Cullen, Murphy & Co., PC</div>
-              <div className="footer-info">1506 Providence Highway, Suite 31 - Norwood. MA 02062</div>
-              <div className="footer-info">(781) 769-6262</div>
-            </div>
         </div>
-
+        <div className="app-footer">
+          <div className="footer-info">Cullen, Murphy & Co., PC</div>
+          <div className="footer-info">1506 Providence Highway, Suite 31 - Norwood. MA 02062</div>
+          <div className="footer-info">(781) 769-6262</div>
+          <Link to='/contact' className="footer-info footer-link">Contact Us</Link>
+        </div>
       </div>
     );
   }
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -3,9 +3,38 @@ import React, { Component } from 'react';
 export default class NotFound extends Component {
   render() {
     return (
-        <p className="App-intro">
-          Friendly 404 page.
-        </p>
+      <div className="service">
+        <div className="about-intro">
+          With a focus on excellence in execution, and adapting to current or developing business situations, Cullen Murphy & Co., P.C., combines knowledge and experience to provide the optimum level of resources required to effectively assess its clients’ accounting, tax and consulting issues.  We provide a comprehensive range of services to individuals, private businesses and corporate clients:
+        </div>
+        <div className="service-section about-bios">
+          <div className="about-bios-text">
+            <div className="about-bios-title">
+              ASSURANCE AND TAX
+            </div>
+            <div className="about-bios-desc">
+              <li>Compilation, review and audit engagements</li>
+              <li>AICPA and GAAP compliance testing</li>
+              <li>Corporate tax planning and filings</li>
+              <li>Partnership</li>
+              <li>State</li>
+              <li>Multi-state</li>
+              <li>Federal</li>
+              <li>Personal tax planning and filings</li>
+            </div>
+          </div>
+          <div className="about-bios-text">
+            <div className="about-bios-title">
+              CONSULTING
+            </div>
+            <div className="about-bios-desc">
+              <li>Operational assessments</li>
+              <li>Cash flow model building and utilization</li>
+              <li>Financial statement projections</li>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
