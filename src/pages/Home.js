5dc4d64d30a05c884e1be93cdb26b2d61a010a98
Glitch (bench-near) <none>
2018-11-12 17:19:57 +0000

    update home desc

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,8 +12,8 @@ export default class Home extends Component {
           </Helmet>
           <div className="home-section home-hero">
             <div className="home-hero-text">
-              When it comes to your finances,
-              <br/>we are in your dugout.
+              We understand your small business,
+              <br/>because we are one too.
             </div>
           </div>
           <div className="home-section">
@@ -22,12 +22,12 @@ export default class Home extends Component {
                   <img src={constImg} alt=""/>
               </div>
               <div className= "home-description-text home-section-double">
-                  Cullen Murphy & Co., P.C., heritage of stability, focus and depth stems from the firm’s historically distinctive construction practice.  Advising companies with construction divisions, general contractors, subcontractors and related industries over the past four decades.
+                <strong>Cullen Murphy & Co., P.C.</strong>  provides diversified tax and accounting services to our clients located in Greater Boston, and throughout New England.  The firm’s history of stability, focus and depth stems from the firm’s distinctive professional expertise assisting closely-held companies with their tax and financial requirements.
               </div>
             </div>
             <div className="home-description">
               <div className= "home-description-text home-section-double">
-                  We provide a unique combination of institutional knowledge and real-world skills in assisting companies in the construction market space.  Within this practice, we advise on corporate and transactional issues, succession planning, acquisitions, divestitures assurance and tax compliance.
+                  We provide a unique combination of institutional knowledge and real-world skills in assisting companies and individuals. Within this practice, we advise on corporate and transactional issues, succession planning, acquisitions, divestitures assurance and tax compliance.
               </div>
               <div className="home-description-img home-section-double">
                   <img src={accountImg} alt=""/>
